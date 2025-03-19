95809de74b91d7178d934ade97e2cb24f3b279ae
Khalid Jebbari <khalid.jebbari@gmail.com>
2025-03-19 17:40:19 +0100

    Update home page.tsx: add link to some-page/1

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,10 @@
 import {useTranslations} from 'next-intl';
+import { Link } from "@/i18n/navigation";
 
 export default function IndexPage() {
   const t = useTranslations('IndexPage');
-  return <h1>{t('title')}</h1>;
+  return <main>
+    <h1>{t('title')}</h1>
+    <Link href={`some-page/${1}`}>Go to some-page/1</Link>
+  </main>;
 }
