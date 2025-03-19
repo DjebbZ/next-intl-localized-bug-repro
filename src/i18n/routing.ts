e8d8b28f206e75bc8802f790fcaf890d72e3ef49
Khalid Jebbari <khalid.jebbari@gmail.com>
2025-03-19 17:33:24 +0100

    Update routing.ts, add localized pathnames

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,5 +2,9 @@ import {defineRouting} from 'next-intl/routing';
 
 export const routing = defineRouting({
   locales: ['en'],
-  defaultLocale: 'en'
+  defaultLocale: 'en',
+  pathnames: {
+    "/": "/",
+    "/some-page/[id]": "/some-page[id]"
+  }
 });
